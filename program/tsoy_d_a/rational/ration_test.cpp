ddfa8138c964218e466686ba208abd941443afda
Unknown <dashanyashaa@gmail.com>
2017-09-22 20:14:56 +0300

    tsoy

---
+++ program/tsoy_d_a/rational/ration_test.cpp
@@ -0,0 +1,57 @@
+#include "ration.h"
+#include <iostream>
+#include <sstream>
+
+bool testParse(const std::string& str)
+{
+	using namespace std;
+	istringstream istrm(str);
+	Rational z;
+	istrm >> z;
+	if (istrm.good()) {
+		cout << "Read success: " << str << " -> " << z << endl;
+
+	}
+	else {
+		cout << "Read error : " << str << " -> " << z << endl;
+
+	}
+	return istrm.good();
+}
+
+int main()
+{
+	using namespace std;
+	Rational z(9, 15);
+	cout << z << " + ";
+	Rational z2(3, 3);
+	cout << z2 << " = ";
+	z += z2;
+	cout << z;
+	cout << "\n" << z << " * ";
+	z2 = Rational(1, 2);
+	cout << z2 << " = ";
+	z *= z2;
+	cout << z << "\n";
+	cout << z << " / " << z2 << " = ";
+	z /= z2;
+	cout << z << "\n";
+	cout << "-" << z << " = " << -z;
+	cout << z << "\n";
+	if (z == z2)
+		cout << z << " = " << z2 << "\n";
+	else
+		cout << z << " != " << z2 << "\n";
+	z2 = z;
+	if (z == z2)
+		cout << z << " = " << z2 << "\n";
+	else
+		cout << z << " != " << z2 << "\n";
+
+	testParse("2/");
+	testParse("2/5");
+	testParse("3/pp5");
+	testParse("4 / 6   ");
+	testParse("4 / 6   rrr");
+	return 0;
+}
---
+++ program/tsoy_d_a/rational/rational.cpp
@@ -0,0 +1,209 @@
+#include "ration.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool Rational::operator==(const Rational& rhs) const 
+{ 
+	return (ch == rhs.ch) && (zn == rhs.zn); 
+}
+
+
+bool Rational::operator!=(const Rational& rhs) const 
+{ 
+	return !operator==(rhs); 
+}
+
+
+Rational& Rational::operator+=(const int rhs) 
+{ 
+	return operator+=(Rational(rhs)); 
+}
+
+
+Rational& Rational::operator-=(const int rhs) 
+{ 
+	return operator+=(Rational(rhs)); 
+}
+
+std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs)
+{
+	return rhs.writeTo(ostrm);
+}
+std::istream& operator >> (std::istream& istrm, Rational& rhs)
+{
+	return rhs.readFrom(istrm);
+}
+
+int nod(int a, int b)
+{
+	while (a != b)
+	{
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+	return a;
+}
+int nok(int a, int b)
+{
+	return a*b / nod(a, b);
+}
+
+Rational::Rational(const int n)
+	: ch(n), zn(1) {}
+
+Rational::Rational(const int integer, const int natural)
+	: ch(integer), zn(natural)
+{
+	//if (0 == natural) throw
+	//{
+
+	//}
+}
+
+void Rational::normaliz(Rational& rhs)
+{
+	if (rhs.zn < 0)
+	{
+		rhs.ch *= -1;
+		rhs.zn *= -1;
+	}
+	const int a = nod(rhs.ch, rhs.zn);
+	rhs.ch /= a;
+	rhs.zn /= a;
+}
+
+Rational& Rational::operator+=(const Rational& rhs)
+{
+	int a(rhs.ch);
+	int b(rhs.zn);
+	int k = nok(zn, rhs.zn);
+	if (zn != k)
+	{
+		ch *= k / zn;
+		zn = k;
+	}
+	if (b != k)
+	{
+		a *= (k / b);
+	}
+	ch += a;
+	Rational::normaliz(*this);
+	return *this;
+}
+
+Rational& Rational::operator-=(const Rational& rhs)
+{
+	int a(rhs.ch);
+	int b(rhs.zn);
+	int k = nok(rhs.ch, rhs.zn);
+	if (zn != k)
+	{
+		ch *= (zn / k);
+		zn = k;
+	}
+	if (b != k)
+	{
+		a *= (b / k);
+	}
+	ch -= a;
+	Rational::normaliz(*this);
+	return *this;
+}
+
+Rational operator+(const Rational& r1, const Rational& r2)
+{
+	Rational sum(r1);
+	sum += r2;
+	return sum;
+}
+
+Rational operator-(const Rational& r1, const Rational& r2)
+{
+	Rational raz(r1);
+	raz -= r2;
+	return raz;
+}
+
+Rational operator*(const Rational& r1, const Rational& r2)
+{
+	Rational pr(r1);
+	pr *= r2;
+	return pr;
+}
+
+Rational operator/(const Rational& r1, const Rational& r2)
+{
+	Rational raz(r1);
+	raz /= r2;
+	return raz;
+}
+
+Rational& Rational::operator*=(const int rhs)
+{
+	ch *= rhs;
+	Rational::normaliz(*this);
+	return *this;
+}
+
+Rational& Rational::operator*=(const Rational& rhs)
+{
+	ch *= rhs.ch;
+	zn *= rhs.zn;
+	Rational::normaliz(*this);
+	return *this;
+}
+
+Rational& Rational::operator/=(const int rhs)
+{
+	zn *= rhs;
+	Rational::normaliz(*this);
+	return *this;
+}
+Rational& Rational::operator/=(const Rational& rhs)
+{
+	zn *= rhs.ch;
+	ch *= rhs.zn;
+	Rational::normaliz(*this);
+	return *this;
+}
+
+//const Rational operator-(const Rational& rhs)
+//{
+//	return (rhs * Rational(-1));
+//}
+
+Rational Rational::pov(const Rational& rhs, const int n)
+{
+	Rational r = rhs;
+	for (int i(1); i < n; i++)
+		r *= rhs;
+	return r;
+}
+
+ostream& Rational::writeTo(std::ostream& ostrm) const
+{
+	ostrm << ch << slash << zn;
+	return ostrm;
+}
+
+istream& Rational::readFrom(std::istream& istrm)
+{
+	int integer(0);
+	char slash(0);
+	int natural(0);
+	istrm >> integer >> slash >> natural;
+	if (istrm.good() || istrm.eof() ) 
+		if (Rational::slash == slash) 
+		{
+			ch = integer;
+			zn = natural;
+		} 
+	else {
+		istrm.setstate(std::ios_base::failbit);
+	}	
+	return istrm;
+}
