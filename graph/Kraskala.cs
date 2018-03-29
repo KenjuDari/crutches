906606052240ecda5ac7daf40f46e0d67266d80b
Unknown <dashanyashaa@gmail.com>
2018-03-29 21:29:32 +0300

    .

---
+++ graph/Kraskala.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace dsuCSharp
+{
+    class Edge
+    {
+        public int from = 0, to = 0, w = 0; // пара вершин и вес
+
+        public Edge()
+        {
+        }
+
+        public Edge(int from, int to, int w)
+        {
+            this.from = from;
+            this.to = to;
+            this.w = w;
+        }
+
+        public int compare(Edge o1, Edge o2)
+        {
+            if (o1.w > o2.w)
+                return 1;
+            else if (o1.w < o2.w)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    class DSU
+    {
+        int[] parent;
+        public int count = 0;
+
+        public DSU(int size)
+        {
+            parent = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                parent[i] = -1;
+                count = size;
+            }
+        }
+
+        public void hang(int vertex, int parentNew)
+        {
+            parent[vertex] = parentNew;
+        }
+
+        public int find(int vertex)
+        {
+            int rootsearch = vertex;
+            while (parent[rootsearch] != -1)
+            {
+                rootsearch = parent[rootsearch];
+            }
+            while (parent[vertex] != -1)
+            {
+                int t = parent[vertex];
+                parent[vertex] = rootsearch;
+                vertex = t;
+            }
+            return rootsearch;
+        }
+
+        public void reset(int n)
+        {
+            if (n > parent.Length)
+            {
+                int[] newParent = new int[n];
+                for (int i = 0; i < parent.Length; i++)
+                {
+                    newParent[i] = parent[i];
+                }
+                parent = newParent;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                parent[i] = -1;
+            }
+        }
+
+        public void union(int vertexA, int vertexB)
+        {
+            int jRoot = find(vertexA);
+            int kRoot = find(vertexB);
+            if (jRoot != kRoot)
+            {
+                if (jRoot < kRoot)
+                {
+
+                    int t = jRoot;
+                    jRoot = kRoot;
+                    kRoot = t;
+                }
+                hang(kRoot, jRoot);
+            }
+            count--;
+        }
+
+        public bool check(int vertexA, int vertexB)
+        {
+            int jRoot = find(vertexA);
+            int kRoot = find(vertexB);
+            if (jRoot == kRoot)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            string print = "";
+            for (int i = 0; i < parent.Length; i++)
+            {
+                print += i.ToString() + "--" + parent[i].ToString() + "\n";
+            }
+            return print;
+        }
+
+    }
+    class Kraskala
+    {
+        static void Main(string[] args)
+        {
+            string sizeStr = Console.ReadLine();
+            string[] sizeArray = sizeStr.Split(' ');
+            int vGraph = int.Parse(sizeArray[0]), eFirst = int.Parse(sizeArray[1]);
+
+
+            DSU test = new DSU(vGraph);
+            Edge[] eArray = new Edge[eFirst];
+            int[] mean = new int[3];
+
+            string str = "";
+            for (int i = 0; i < eFirst; i++)
+            {
+                str = Console.ReadLine();
+                string[] strSplit = str.Split(' ');
+                eArray[i] = new Edge(int.Parse(strSplit[0]), int.Parse(strSplit[1]), int.Parse(strSplit[2]));
+            }
+
+            Array.Sort(eArray);
+
+            bool[] eTrue = new bool[eFirst];
+            for (int i = 0; i < eFirst; i++)
+            {
+                eTrue[i] = false;
+            }
+
+            Edge e = new Edge();
+            for (int i = 0; i < eFirst; i++)
+            {
+                e = eArray[i];
+
+                if (test.check(e.from, e.to))
+                {
+                    test.union(e.from, e.to);
+                    eTrue[i] = true;
+                    Console.WriteLine(e.from + "--" + e.to + ";");
+                }
+                if (test.count == 1) break;
+
+            }
+        }
+    }
+}
---
+++ graph/heapProg.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heapFull
+{
+    class Heap  // c min в корне
+    {
+        int[] arrayHeap;
+        int countV;
+        public Heap(int[] values)
+        {
+            countV = values.Length;
+            arrayHeap = new int[countV + 1];  // массив заполняется начиная с 1 индекса
+            arrayHeap[0] = 0;
+            for (int i = 1; i <= countV; i++)
+            {
+                arrayHeap[i] = values[i - 1];
+            }
+            heapify();
+        }
+
+        void remontOut( int v)          // наружный ремонт при увелечении элемента с интексом v
+        {
+            int indexChild1 = 0;
+            int indexChild2 = 0;
+            for (int indexCurrent = v; indexCurrent <= countV / 2; indexCurrent = indexChild1)  // от текущего элемента до уровня перед листьями (предпоследний)
+            {
+                indexChild1 = 2 * indexCurrent;
+                indexChild2 = 2 * indexCurrent + 1;
+                if ((indexChild2 <= countV) && (arrayHeap[indexChild2] < arrayHeap[indexChild1]))  // второй ребенок есть (индекс меньше последнего)
+                                                                                            // второй ребенок меньше первого
+                {
+                    indexChild1 = indexChild2; // работаем с меньшим из потомков
+                }
+                if (indexChild1 > countV) // потомки закончились
+                {
+                    break;
+                }
+                if (arrayHeap[indexCurrent] < arrayHeap[indexChild1]) // соотношение порядка соблюдено
+                {
+                    break;
+                }
+                                                                             // если нрушен порядок, меняем текущей элемент с потомком
+                int t = arrayHeap[indexCurrent];
+                arrayHeap[indexCurrent] = arrayHeap[indexChild1];
+                arrayHeap[indexChild1] = t;
+            }
+        }
+
+        void remontIn (int v)            // внутренний ремонт при уменьшении элемента с интексом v
+        {
+            int parent = 0;
+            for (int indexCurrent = v; indexCurrent > 1; indexCurrent = parent)
+            {
+                parent = indexCurrent / 2;
+                if (arrayHeap[indexCurrent] > arrayHeap[parent])
+                {
+                    break;
+                }
+
+                int t = arrayHeap[indexCurrent];
+                arrayHeap[indexCurrent] = arrayHeap[parent];
+                arrayHeap[parent] = t;
+            }
+        }
+
+        public void add (int newElement)
+        {
+            if (countV >= arrayHeap.Length - 1)
+            {
+                int[] arrayHeapNew = new int[arrayHeap.Length + 1];
+                Array.Copy(arrayHeap, arrayHeapNew, arrayHeap.Length);
+                arrayHeap = arrayHeapNew;
+
+            }
+            arrayHeap[++countV] = newElement;
+            remontIn(countV);
+        }      
+
+        public void delete(int indexDel)
+        {
+            int elementDel = arrayHeap[indexDel];
+            arrayHeap[indexDel] = arrayHeap[countV--];
+            if (elementDel > arrayHeap[indexDel])
+            {
+                remontIn(indexDel);
+            }
+            else
+            {
+                remontOut(indexDel);
+            }
+        }
+
+        int deleteRoot()
+        {
+            int valueRoot = arrayHeap[1];
+            arrayHeap[1] = arrayHeap[countV--];
+            remontOut(1);
+            return valueRoot;
+        }
+
+        void heapify()
+        {
+            for (int indexCurrent = countV/2; indexCurrent > 0; indexCurrent--)
+            {
+                remontOut(indexCurrent);
+            }
+        }
+
+        public void pyramidSort()
+        {
+            int countVRemember = countV;
+            while (countV > 0)
+            {
+                arrayHeap[countV] = deleteRoot();
+            }
+            countV = countVRemember;
+        }
+
+        public void writeArray()
+        {
+            for (int i = 1; i < countV; i++)
+            {
+                Console.Write(arrayHeap[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public override string ToString()
+        {
+            string print = "";
+            for (int i = 1; i < countV / 2; i++)
+            {
+                print += arrayHeap[i].ToString() + "->" + arrayHeap[i * 2].ToString() + "\n";
+                if (i * 2 + 1 <= countV)
+                {
+                    print += arrayHeap[i].ToString() + "->" + arrayHeap[i * 2 + 1].ToString() + "\n";
+                }
+            }
+            return print;
+        }
+    }
+
+    class heapProg
+    {
+        static void Main(string[] args)
+        {
+            Heap test = new Heap(new int[] {4, 5, 234, 10, 67, 21, 0, -6, -2, -10, 100 });
+            test.add(-100);
+            test.delete(5);
+            Console.WriteLine(test);
+            test.pyramidSort();
+            test.writeArray();
+
+        }
+    }
+}
